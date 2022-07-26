41585d4451dbc33dc3d70479b94db8120fa58168
Brian Martínez Rodríguez <44876509+brianmrdev@users.noreply.github.com>
2022-07-26 11:24:54 -0400

    Migración del metodo de consulta mediante API

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 # Ponemos nuestro Token generado con el @BotFather
 TOKEN = " "
 
-# Configuracion de la conexion a la base datos
-hostname = ' '
-username = ' '
-password = ' '
-database = ' '
-port = ' '
+# URL API
+URL = ''
